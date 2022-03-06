bf0e5ff3c7ca99ef41bc6e8628a4f28cb8cd3930
jjhmero8659 <77915673+jjhmero8659@users.noreply.github.com>
2022-03-06 17:47:17 +0900

    Add files via upload

--- App.js
+++ App.js
@@ -1,57 +1,97 @@
-import React , {Component} from "react";
+import logo from './logo.svg';
 import './App.css';
+import React , {Component} from "react";
 import axios from "axios";
 
 class App extends Component{
   constructor(props){
-    super(props)
-    this.state={
-      app_data:"",
-      app_data2:"",
+    super(props);
+
+    this.state = {
+      text:"",
     }
   }
 
   request1 = async() =>{
-    const res = await axios.get("/hello");
+    alert("요청.before");
+    const result = await axios.get("/hello");
+    console.log(result.data.hello);
     this.setState({
-      app_data:res.data.hello
+      text : result.data.hello,
     })
   }
 
-  request2 = async() =>{
-    const res = await axios.get("/bye");
+  buy_request = async() =>{
+    alert("buy");
+    const result = await axios.get("/buy");
+  }
+
+  req_person1 = async() =>{
+    const result = await axios.get("/person1" + "이순신");
+    console.log(result.data.find)
     this.setState({
-      app_data:res.data.bye
+      // text: result.data.find
     })
   }
 
-  request3 = async() =>{
-    const res = await axios.get("/movies1"+130);
+  req_person2 = async() =>{
+    const result = await axios.get("/person2"+"홍길동"+"&"+27);
   }
 
-  request4 = async() =>{
-    const res = await axios.get("/movies2"+130+"&"+"tank");
+  req_person3 = async() =>{
+    const result = await axios.get("/person3"+"척준경"+"&"+25+"&"+185);
     this.setState({
-      app_data : res.data.text //응답한 data 중 text의 값을 app_data에 저장
+      text: result.data.name + " " +  result.data.age + " " + result.data.height, 
     })
   }
 
-  request5 = async() =>{
-    const res = await axios.get("/movies3"+130+"&"+"tank"+"&"+"strong");
+  req_person4 = async() =>{
+    const personObj = {name:"송중기", age:21, height:176.5};
+    const result = await axios.post("/person4",personObj);
     this.setState({
-      app_data:res.data.send_message
+      
     })
   }
+
+  req_person5 = async() =>{
+    const person_name = {name : "정채연"}
+    const result = await axios.delete("/person5"+"정채연");
+    this.setState({
+      
+    })
+  }
+
+  req_person6 = async() =>{
+    const updateData = {age:30, height:165.2}
+    const result = await axios.put("/person6"+"정채연",updateData);
+    this.setState({
+      
+    })
+  }
+
+  req_person7 = async() =>{
+    const updateData = {age:30}
+    const result = await axios.patch("/person7"+"정채연",updateData);
+    this.setState({
+      
+    })
+  }
+
+
 
   render(){
     return(
       <div>
-        <h1>{this.state.app_data}</h1>
-        <button onClick={this.request1}>요청 1</button>
-        <button onClick={this.request2}>요청 2</button>
-        <button onClick={this.request3}>요청 3</button>
-        <button onClick={this.request4}>요청 4</button>
-        <button onClick={this.request5}>요청 5</button>
+        <h1>{this.state.text}</h1>
+        <button onClick={this.request1}>request</button>
+        <button onClick={this.buy_request}>buy</button>
+        <button onClick={this.req_person1}>req_person1</button>
+        <button onClick={this.req_person2}>req_person2</button>
+        <button onClick={this.req_person3}>req_person3</button>
+        <button onClick={this.req_person4}>post</button>
+        <button onClick={this.req_person5}>delete</button>
+        <button onClick={this.req_person6}>put</button>
+        <button onClick={this.req_person7}>patch</button>
       </div>
     )
   }
--- server/server.js
+++ server/server.js
@@ -1,45 +1,149 @@
-//백엔드
-
 const express = require("express")
 
 const app = express()
 
-const PORT = process.env.PORT || 4000; //port 번호를 4000번으로 사용하겠다
+const PORT = process.env.PORT || 4000;
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+var personList = [
+    {name:"이민호",age:20,height:176.3},
+    {name:"송중기",age:21,height:175.2},
+    {name:"정채연",age:22,height:162.5}
+]
 
 app.get("/hello",(req,res) =>{
-    console.log("get hello")
-    res.send({hello : "response hello"}) //response.send - 서버의 응답을 위한 함수
+    //req = request = 넘어온 요청 관련
+    //res = response = 서버 응답 관련
+    console.log("get hello");
+    res.send({hello : "My name is hello"});
 })
 
-app.get("/bye",(req,res) =>{
-    console.log("get bye")
-    res.send({bye : "response bye"})
+app.get("/buy",(req,res) =>{
+    console.log("get buy");
 })
 
-app.get("/movies1:id",(req,res) =>{
-    console.log("/movies:id")
+app.get("/person1:name",(req,res) =>{
+    console.log(req.params.name);
+    const name = req.params.name
+    let findPersonObj = null
+    //요청에 붙여서 넘어온 파라미터중 name 값을 꺼냄
+    //요청받은 name 값에 해당하는 객체를 찾아서 출력
+
+    for(var i=0; i<personList.length; i++){
+        if(personList[i].name = name){
+            console.log("이름:"+personList[i].name)
+            console.log("나이:"+personList[i].age)
+            console.log("키:"+personList[i].height)
+            findPersonObj = {
+                name : personList[i].name,
+                age : personList[i].age,
+                height : personList[i].height
+            }
+        }
+    }
+    res.send({find:findPersonObj});
+})
+
+app.get("/person2:name&:age",(req,res) =>{
+    console.log(req.params.name);
+    console.log(req.params.age);
+})
+
+
+app.get("/person3:name&:age&:height",(req,res) =>{
+    console.log(req.params.name);
+    console.log(req.params.age);
+    console.log(req.params.height);
+    // res.send({name:req.params.id}); // res 는 한번에 한개씩만 전달 해야함
+    // res.send({age:req.params.age});
+    // res.send({height:req.params.height});
+    res.send({name:req.params.name ,age:req.params.age , height: req.params.height})
+})
+
+app.post("/person4",(req,res) =>{
+    console.log("person")
+    console.log(req.body);
+    console.log(req.body.name);
+    console.log(req.body.age);
+
+    const name = req.body.name;
+    const age = req.body.age;
+    const height = req.body.height;
+
+    const personObj = {name:name , age:age , height:height}
+    personList = personList.concat(personObj)
+    for(var i=0; i<personList.length; i++){
+        console.log("이름:"+personList[i].name)
+        console.log("나이:"+personList[i].age)
+        console.log("키:"+personList[i].height)
+    }
+})
+
+app.delete("/person5:name",(req,res) =>{
+    console.log("person5");
     console.log(req.params)
-    console.log(req.params.id)
+    console.log(req.params.name)
+    const name = req.params.name;
+
+    personList = personList.filter(  
+        (data)=>(data.name == name)
+    )
+    //name 값이 넘겨받은 값이랑 다른 것만 통과
+    //filter 리턴 하는 배열값은 name 이 아닌것만 return
+    //조건을 만족시키는 원소 배열 만으로 재생성
+
+    //filter 메서드 삭제
+    //배열 관련 메서드
+    //삭제 관련 함수 : unshift,pop,slice,splice,filter
+
+    for(var i=0; i<personList.length; i++){
+        console.log("이름:"+personList[i].name)
+        console.log("나이:"+personList[i].age)
+        console.log("키:"+personList[i].height)
+    }
 })
 
-app.get("/movies2:id&:title",(req,res) =>{
-    console.log("/movies:id&:title")
-    console.log(req.params)
-    console.log(req.params.id)
-    res.send({text:"params.id : " + req.params.id})
+app.put("/person6:name",(req,res) =>{
+    console.log("person6");
+    console.log(req.params) //정채연
+    console.log(req.body)   //수정할 정보
+    
+    //배열에 정보를 찾아서 변경
+    //전체 출력 (변경된것 확인) 
+    var name = req.params.name;
+
+    for(var i=0; i<personList.length; i++){
+        if(personList[i].name == "정채연"){
+            personList[i].age = req.body.age;
+            personList[i].height = req.body.height;
+        }
+        console.log("이름:"+personList[i].name)
+        console.log("나이:"+personList[i].age)
+        console.log("키:"+personList[i].height)
+    }
 })
 
-app.get("/movies3:id&:title&:strong",(req,res) =>{
-    //서버가 요청을 받아들임...
-    console.log("/movies:id&:title&:strong")
-    console.log(req.params)
-    console.log(req.params.id)
-    var id = req.params.title
-    res.send({send_message:"response : " + id})
-    //response.send
-    //서버의 응답
+app.patch("/person7",(req,res) =>{
+    console.log("person7");
+
+    console.log(req.params) //정채연
+    console.log(req.body)   //수정할 정보
+    //배열에 정보를 찾아서 변경
+
+    var name = req.params.name;
+
+    for(var i=0; i<personList.length; i++){
+        if(personList[i].name == "정채연"){
+            personList[i].age = req.body.age;
+        }
+        console.log("이름:"+personList[i].name)
+        console.log("나이:"+personList[i].age)
+        console.log("키:"+personList[i].height)
+    }
 })
 
-app.listen(PORT, ()=>{
-    console.log(`Server On:http://localhost: ${PORT}`)
+
+app.listen(PORT,()=>{
+    console.log(`Server On : http://localhost:${PORT}`)
 })
